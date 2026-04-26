b6bcf1ff85ec6ca14ee2dd9ea72c826fdd976563
Phạm Lê Trung <plt@netcompany.com>
2026-04-26 23:18:51 +0700

    feat: update study

--- dartpedia/wikipedia/lib/src/api/summary.dart
+++ dartpedia/wikipedia/lib/src/api/summary.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -30,5 +31,27 @@ Future<Summary> getRandomArticleSummary() async {
 }
 
 Future<Summary> getArticleSummaryByTitle(String articleTitle) async {
-  
+  final http.Client client = http.Client();
+  try {
+    final Uri url = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/summary/$articleTitle',
+    );
+    final http.Response response = await client.get(url);
+    if (response.statusCode == 200) {
+      final Map<String, Object?> jsonData =
+          jsonDecode(response.body) as Map<String, Object?>;
+      return Summary.fromJson(jsonData);
+    } else {
+      throw HttpException(
+        '[WikipediaDart.getArticleSummary] '
+        'statusCode=${response.statusCode}, body=${response.body}',
+      );
+    }
+  } on FormatException {
+    // todo: log exceptions
+    rethrow;
+  } finally {
+    client.close();
+  }
 }
